f0665b9ceabf533ab20c777b4ca8c313dfb8d36c
Jose Ortiz <joseortiz1807@gmail.com>
2025-02-06 11:18:54 -0500

    ✨ Add file for automatic installation

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+INSTALL_DIR="$HOME/bin"
+VENV_DIR="$INSTALL_DIR/pomo-env"
+
+echo "Creating $INSTALL_DIR if it doesn't exist..."
+mkdir -p "$INSTALL_DIR"
+
+echo "Moving scripts to $INSTALL_DIR..."
+cp pomodoro.sh pomo_plot.sh pomo_plot.py "$INSTALL_DIR/"
+
+echo "Setting execution permissions..."
+chmod +x "$INSTALL_DIR/pomodoro.sh"
+chmod +x "$INSTALL_DIR/pomo_plot.sh"
+
+echo "Creating virtual environment in $VENV_DIR..."
+python3 -m venv "$VENV_DIR" # TODO: breaks if no "proper" python3 version installed, or no venv
+
+echo "Installing dependencies..."
+source "$VENV_DIR/bin/activate"
+pip install -r requirements.txt
+
+echo "Installation complete! You can now run your Pomodoro scripts."
